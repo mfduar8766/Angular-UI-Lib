cb8315f1aa04a1dcd1f0145a25c93bee72de01fc
Matheus Duarte <mfduar8766@gmail.com>
2020-05-03 19:53:47 -0400

    Added new features to common table

--- src/lib/common-table/common-table.component.ts
+++ src/lib/common-table/common-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'common-table',
@@ -6,13 +8,69 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./common-table.component.css']
 })
 export class CommonTableComponent implements OnInit {
-  @Input() tableData;
-  @Input() columns;
+  @Input() dataKey: string;
+  @Input() tableData: any[];
+  @Input() columns: any[];
   @Input() rowsperPage = 5;
   @Input() showPagination = true;
   @Input() rowsPerPageOptions: number[] = [5, 10, 25, 50, 100];
+  @Input() styleClass: string;
+  @Output() editTableData: EventEmitter<any> = new EventEmitter();
+  showDeleteMessage: boolean;
+  selectedData: any[];
+  totalRecords: number;
+  first = 0;
+  rowsPerPageSelection = [
+    { label: 5, value: 5 },
+    { label: 10, value: 10 },
+    { label: 25, value: 25 },
+    { label: 100, value: 100 }
+  ];
 
-  constructor() {}
+  constructor(private confirmationService: ConfirmationService, private messageService: MessageService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.tableData && this.tableData.length) {
+      this.totalRecords = this.tableData.length;
+    }
+  }
+
+  nextPage() {
+    this.first = this.first + this.rowsperPage;
+  }
+
+  previousPage() {
+    this.first = this.first - this.rowsperPage;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  isLastPage(): boolean {
+    return this.first === this.tableData.length - this.rowsperPage;
+  }
+
+  isFirstPage(): boolean {
+    return this.first === 0;
+  }
+
+  setRowsPerPage(event) {
+    this.rowsperPage = event.value;
+  }
+
+  deleteData(data) {
+    this.confirmationService.confirm({
+      message: 'Do you want to delete this record?',
+      header: 'Delete Record',
+      icon: 'fa fa-info-circle',
+      accept: () => {
+        this.tableData = this.tableData.filter(dataValue => dataValue.agencyId !== data.agencyId);
+        this.messageService.add({ severity: 'success', key: 'deleteSuccess', summary: 'Record deleted.' });
+      },
+      reject: () => {
+        return;
+      }
+    });
+  }
 }
