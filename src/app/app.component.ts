4eb078b0411118387e9f84fb5e420aa85bf782e4
Matheus Duarte <mfduar8766@gmail.com>
2020-05-10 21:40:51 -0400

    Replaces css with scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,7 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit, AfterViewInit, AfterViewChecked {
   @ViewChild(DynamicFormComponent) form: DynamicFormComponent;
