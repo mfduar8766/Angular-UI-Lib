429550ca7f998d7ffbc0838b6fa9fa19e8cbf74b
Matheus Duarte <mfduar8766@gmail.com>
2020-05-17 11:08:25 -0400

    Changed auto complete and select components

--- src/lib/common-auto-complete/common-search.module.ts
+++ src/lib/common-auto-complete/common-search.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PrimeNgModule } from '../PrimeNgModules.module';
 import { CommonSearchComponent } from './common-search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, PrimeNgModule],
+  imports: [CommonModule, FormsModule, PrimeNgModule],
   declarations: [CommonSearchComponent],
   exports: [CommonSearchComponent]
 })
